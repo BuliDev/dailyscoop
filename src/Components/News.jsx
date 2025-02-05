3674e8e45abb61e70f4cedc5c7e7e1fd92638b75
BuliDev <contact@alenburovic.com>
2025-02-05 14:55:29 +0100

    Add Bookmarks Modal Box & Functionality

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,146 +1,188 @@
-import React, { useEffect, useState } from 'react'
-import Weather from './Weather'
-import Calendar from './Calendar'
-import './News.css'
-import userImg from '../assets/images/user.jpg'
-import noImg from '../assets/images/no-img.png'
-import axios from 'axios'
-import NewsModal from './NewsModal'
+import React, { useEffect, useState } from "react";
+import Weather from "./Weather";
+import Calendar from "./Calendar";
+import "./News.css";
+import userImg from "../assets/images/user.jpg";
+import noImg from "../assets/images/no-img.png";
+import axios from "axios";
+import NewsModal from "./NewsModal";
+import Bookmarks from "./Bookmarks";
 
 const categories = [
-  'general',
-  'world',
-  'business',
-  'technology',
-  'entertainment',
-  'sports',
-  'science',
-  'health',
-  'nation',
-]
+  "general",
+  "world",
+  "business",
+  "technology",
+  "entertainment",
+  "sports",
+  "science",
+  "health",
+  "nation",
+];
 
 const News = () => {
-  const [headline, setHeadline] = useState(null)
-  const [news, setNews] = useState([])
-  const [selectedCategory, setSelectedCategory] = useState('general')
-  const [searchInput, setSearchInput] = useState('')
-  const [searchQuery, setSearchQuery] = useState('')
-  const [showModal, setShowModal] = useState(false)
-  const [selectedArticle, setSelectedArticle] = useState(null)
+  const [headline, setHeadline] = useState(null);
+  const [news, setNews] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("general");
+  const [searchInput, setSearchInput] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [showModal, setShowModal] = useState(false);
+  const [selectedArticle, setSelectedArticle] = useState(null);
+  const [bookmarks, setBookmarks] = useState([]);
+  const [showBookmarksModal, setShowBookmarksModal] = useState(false);
 
   useEffect(() => {
     const fetchNews = async () => {
-      const apiKey = import.meta.env.VITE_GNEWS_API_KEY
+      const apiKey = import.meta.env.VITE_GNEWS_API_KEY;
 
-      let url = `https://gnews.io/api/v4/top-headlines?category=${selectedCategory}&lang=en&apikey=${apiKey}`
+      let url = `https://gnews.io/api/v4/top-headlines?category=${selectedCategory}&lang=en&apikey=${apiKey}`;
 
       if (searchQuery) {
-        url = `https://gnews.io/api/v4/search?q=${searchQuery}&lang=en&apikey=${apiKey}`
+        url = `https://gnews.io/api/v4/search?q=${searchQuery}&lang=en&apikey=${apiKey}`;
       }
 
-      const response = await axios.get(url)
-      const fetchedNews = response.data.articles
+      const response = await axios.get(url);
+      const fetchedNews = response.data.articles;
 
       fetchedNews.forEach((article) => {
         if (!article.image) {
-          article.image = noImg
+          article.image = noImg;
         }
-      })
+      });
 
-      setHeadline(fetchedNews[0])
-      setNews(fetchedNews.slice(1, 7))
-    }
+      setHeadline(fetchedNews[0]);
+      setNews(fetchedNews.slice(1, 7));
+    };
 
-    fetchNews()
-  }, [selectedCategory, searchQuery])
+    fetchNews();
+  }, [selectedCategory, searchQuery]);
 
   const handleCategory = (e, category) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    setSelectedCategory(category)
-  }
+    setSelectedCategory(category);
+  };
 
   const handleSearch = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    setSearchQuery(searchInput)
-    setSearchInput('')
-  }
+    setSearchQuery(searchInput);
+    setSearchInput("");
+  };
 
   const handleArticleClick = (article) => {
-    setSelectedArticle(article)
-    setShowModal(true)
-  }
+    setSelectedArticle(article);
+    setShowModal(true);
+  };
+
+  const handleBookmarkClick = (article) => {
+    setBookmarks((prevBookmarks) => {
+      const updatedBookmarks = prevBookmarks.find(
+        (bookmark) => bookmark.title === article.title
+      )
+        ? prevBookmarks.filter((bookmark) => bookmark.title !== article.title)
+        : [...prevBookmarks, article];
+      return updatedBookmarks;
+    });
+  };
 
   return (
-    <div className='news'>
-      <header className='news-header'>
-        <h1 className='logo'>Daily Scoop</h1>
-        <div className='search-bar'>
+    <div className="news">
+      <header className="news-header">
+        <h1 className="logo">Daily Scoop</h1>
+        <div className="search-bar">
           <form onSubmit={handleSearch}>
             <input
-              type='text'
-              placeholder='Search News...'
+              type="text"
+              placeholder="Search News..."
               value={searchInput}
               onChange={(e) => setSearchInput(e.target.value)}
             />
-            <button type='submit'>
-              <i className='fa-solid fa-magnifying-glass'></i>
+            <button type="submit">
+              <i className="fa-solid fa-magnifying-glass"></i>
             </button>
           </form>
         </div>
       </header>
-      <div className='news-content'>
-        <div className='navbar'>
-          <div className='user'>
-            <img src={userImg} alt='User Image' />
+      <div className="news-content">
+        <div className="navbar">
+          <div className="user">
+            <img src={userImg} alt="User Image" />
             <p>Alen's Blog</p>
           </div>
-          <nav className='categories'>
-            <h1 className='nav-heading'>Categories</h1>
-            <div className='nav-links'>
+          <nav className="categories">
+            <h1 className="nav-heading">Categories</h1>
+            <div className="nav-links">
               {categories.map((category) => (
                 <a
                   key={category}
-                  href='#'
-                  className='nav-link'
+                  href="#"
+                  className="nav-link"
                   onClick={(e) => handleCategory(e, category)}
                 >
                   {category}
                 </a>
               ))}
 
-              <a href='#' className='nav-link'>
-                Bookmarks <i className='fa-regular fa-bookmark'></i>
+              <a
+                href="#"
+                className="nav-link"
+                onClick={() => setShowBookmarksModal(true)}
+              >
+                Bookmarks <i className="fa-solid fa-bookmark"></i>
               </a>
             </div>
           </nav>
         </div>
-        <div className='news-section'>
+        <div className="news-section">
           {headline && (
             <div
-              className='headline'
+              className="headline"
               onClick={() => handleArticleClick(headline)}
             >
               <img src={headline.image || noImg} alt={headline.title} />
-              <h2 className='headline-title'>
+              <h2 className="headline-title">
                 {headline.title}
-                <i className='fa-regular fa-bookmark bookmark'></i>
+                <i
+                  className={`${
+                    bookmarks.some(
+                      (bookmark) => bookmark.title === headline.title
+                    )
+                      ? "fa-solid"
+                      : "fa-regular"
+                  } fa-bookmark bookmark`}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleBookmarkClick(headline);
+                  }}
+                ></i>
               </h2>
             </div>
           )}
 
-          <div className='news-grid'>
+          <div className="news-grid">
             {news.map((article, index) => (
               <div
                 key={index}
-                className='news-grid-item'
+                className="news-grid-item"
                 onClick={() => handleArticleClick(article)}
               >
                 <img src={article.image || noImg} alt={article.title} />
                 <h3>
                   {article.title}
-                  <i className='fa-regular fa-bookmark bookmark'></i>
+                  <i
+                    className={`${
+                      bookmarks.some(
+                        (bookmark) => bookmark.title === article.title
+                      )
+                        ? "fa-solid"
+                        : "fa-regular"
+                    } fa-bookmark bookmark`}
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      handleBookmarkClick(article);
+                    }}
+                  ></i>
                 </h3>
               </div>
             ))}
@@ -151,15 +193,22 @@ const News = () => {
           article={selectedArticle}
           onClose={() => setShowModal(false)}
         />
-        <div className='my-blogs'>My Blogs</div>
-        <div className='weather-calendar'>
+        <Bookmarks
+          show={showBookmarksModal}
+          bookmarks={bookmarks}
+          onClose={() => setShowBookmarksModal(false)}
+          onSelectArticle={handleArticleClick}
+          onDeleteBookmark={handleBookmarkClick}
+        />
+        <div className="my-blogs">My Blogs</div>
+        <div className="weather-calendar">
           <Weather />
           <Calendar />
         </div>
       </div>
-      <footer className='news-footer'>Footer</footer>
+      <footer className="news-footer">Footer</footer>
     </div>
-  )
-}
+  );
+};
 
-export default News
+export default News;
