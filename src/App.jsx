d4caedf5141ee879df41272e3f5a2217dd45cc0c
BuliDev <contact@alenburovic.com>
2025-02-07 20:53:47 +0100

    Create & Customize Blogs Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import News from './Components/News'
+import React from "react";
+import News from "./Components/News";
+import Blogs from "./Components/Blogs";
 
 const App = () => {
   return (
-    <div className='container'>
-      <div className='news-blog-app'>
-        <News />
+    <div className="container">
+      <div className="news-blogs-app">
+        {/* <News /> */}
+        <Blogs />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
